b7d9e732a3dce83256b7afc45bae2fd51ace1666
kibigo <jacksonkibigo99@gmail.com>
2023-11-26 00:11:46 +0300

    added login/logout

--- Flask-Auth/app.py
+++ Flask-Auth/app.py
@@ -1,19 +1,34 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_migrate import Migrate
 from models import db
-from models import LoginForm, RegisterForm
+from models import LoginForm, RegisterForm, User
+from flask_bcrypt import Bcrypt
 
 
 
 app = Flask(__name__)
 
+bcrypt = Bcrypt(app)
 
 migrate = Migrate(app, db)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 
 db.init_app(app)
 
@@ -21,17 +36,52 @@ db.init_app(app)
 def home ():
     return render_template('home.html')
 
+
+
 @app.route('/login', methods = ['GET', 'POST'])
 def login ():
 
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+
+                return redirect(url_for('dashboard'))
 
     return render_template('login.html', form = form)
+
+
+
+@app.route('/dashboard', methods =['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+
+@app.route('/logout', methods = ['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+
+    return redirect(url_for('login'))
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
 
     form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password = hashed_password)
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
 
     return render_template('register.html', form = form)
 
